41f14c26ecc797ff892f02a97526f43ab6bac429
gannitskyI <ganniczky@yandex.ru>
2024-08-30 15:45:46 +0300

    new

--- My project (2)/Assets/Scripts/RotateImage.cs
+++ My project (2)/Assets/Scripts/RotateImage.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class RotateImage : MonoBehaviour
 {
-    public float rotationSpeed = 10f; // Скорость вращения
+    public float rotationSpeed = 10f; 
 
     void Update()
-    {
-        // Вращаем объект по оси Z на указанную скорость
+    { 
         transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
     }
 }
